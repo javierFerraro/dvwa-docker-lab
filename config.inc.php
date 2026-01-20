55b71c1b1200634a2e710121c4335f9f517595a9
javierFerraro <javi.ferraro@hotmail.com>
2026-01-20 16:05:34 -0300

    Create config.inc.php

---
+++ config.inc.php
@@ -0,0 +1,16 @@
+
+<?php
+
+$_DVWA = array();
+$_DVWA['db_server']   = 'localhost';
+$_DVWA['db_database'] = 'dvwa';
+$_DVWA['db_user']     = 'dvwa';
+$_DVWA['db_password'] = 'password';
+$_DVWA['db_port']     = '3306';
+
+$_DVWA['recaptcha_public_key']  = '';
+$_DVWA['recaptcha_private_key'] = '';
+
+$_DVWA['default_security_level'] = 'low';
+?>
+``
